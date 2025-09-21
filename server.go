b9ff3f1924037c9bcf3972a29c3e654836b4c37b
vijayarun00100 <blockloader78@gmail.com>
2025-09-22 00:07:31 +0530

    Enhanced CORS headers for better browser compatibility

--- server.go
+++ server.go
@@ -55,7 +55,9 @@ func main() {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Max-Age", "86400")
 			
 			if r.Method == "OPTIONS" {
 				w.WriteHeader(http.StatusOK)
